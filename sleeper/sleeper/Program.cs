8d37f4523302ab7575779544158f8f4ac44130e6
Giovanni Giorgi <giovanni.giorgi@nttdata.com>
2014-09-22 14:37:16 +0200

    Program to sleep milliseconds: used to test the data pusher

---
+++ sleeper/sleeper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sleeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Sleep:" + args[0]);
+            int v=Int32.Parse(args[0]);
+            System.Threading.Thread.Sleep(v);
+        }
+    }
+}
